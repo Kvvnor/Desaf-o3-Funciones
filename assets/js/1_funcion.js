1be5c28737beb899719133b6581084a12469c9b3
Kevin Ortiz <kvvinortiz@gmail.com>
2025-05-12 22:45:08 -0400

    Aplico declaración de función a una función de expresión

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,10 @@
+// 1. Transforma esta declaración de función a una función de expresión. El ejercicio debe
+// quedar en un archivo nombrado
+
+const example = function (a, b, c) {
+    return a + b + c;
+}
+
+console.log(example(1, 2, 3)); // 6
+
+
